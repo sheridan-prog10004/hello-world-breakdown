08a8b8a191cfe32622751d685b48913024372b8e
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-09-10 13:17:10 -0400

    Version 3: Defining and calling a function

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,12 @@
-helloMsg = 'Hello Programming World'
-print(helloMsg)
+def sayHello():
+    helloMsg = 'Hello Programming World'
+    print(helloMsg)
+    
+    courseMsg = "Programming Principles with Python"
+    print(courseMsg)
+    
+    courseNo = 10004
+    print('#prog', courseNo, sep='')
 
-courseMsg = "Programming Principles with Python"
-print(courseMsg)
-
-courseNo = 10004
-print('PROG', courseNo, sep='')
+#use the word we have defined above to be a function, sayHello()
+sayHello()
